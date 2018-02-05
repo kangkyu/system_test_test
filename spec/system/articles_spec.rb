0fce88cd76ff23cf387874cc643383724bec7ddb
Kang-Kyu Lee <kangkyu1111@gmail.com>
2018-02-05 14:49:05 -0800

    Add another rspec example

--- spec/system/articles_spec.rb
+++ spec/system/articles_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe 'Homepage' do
+RSpec.describe 'Homepage', type: :system do
   before do
-    driven_by :selenium_chrome_headless
+    driven_by :rack_test
   end
 
   it "shows greeting" do
     visit root_url
     expect(page).to have_content 'Hello!'
+  end
+
+  it 'enables me create articles' do
+    visit '/articles/new'
+
+    fill_in 'Title', with: "My Article"
+    click_button "Create Article"
+
+    expect(page).to have_text("Article was successfully created.")
   end
 end
